6c467cf7fb7d34c828e45b605ced242128823177
Alvin Sun <adonissun@msn.cn>
2014-11-26 13:47:27 -0700

    InitEverything
    Init Events

---
+++ ScrolTest/ScrolTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrolTest
+//
+//  Created by Alvin Sun on 2014-11-26.
+//  Copyright (c) 2014 Alvin Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
